b9b1200538441e3c1bfb9afc1111aaa37625e785
Ömür BURUK <burukomur@gmail.com>
2017-12-03 19:49:10 +0300

    DB Script
    veritabanı scripti eklendi

---
+++ ebrubayburtlu_com.sql
@@ -0,0 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 03 Ara 2017, 17:27:14
+-- Sunucu sürümü: 10.1.21-MariaDB
+-- PHP Sürümü: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `ebrubayburtlu.com`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `label`
+--
+
+CREATE TABLE `label` (
+  `labelid` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Tablo döküm verisi `label`
+--
+
+INSERT INTO `label` (`labelid`, `name`, `value`) VALUES
+(1, 'info', 'Ben Ebru Bayburtlu. Bu sitenin sahibiyim.'),
+(2, 'mail', 'info@ebrubayburtlu.com');
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `projects`
+--
+
+CREATE TABLE `projects` (
+  `ProjectId` int(11) NOT NULL,
+  `ProjectTypeId` int(11) NOT NULL,
+  `Header` varchar(100) NOT NULL,
+  `Director` varchar(100) NOT NULL,
+  `With` varchar(300) NOT NULL,
+  `Cover` text NOT NULL,
+  `VideoPath` text NOT NULL,
+  `CreateDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `projecttype`
+--
+
+CREATE TABLE `projecttype` (
+  `ProjectTypeId` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `label`
+--
+ALTER TABLE `label`
+  ADD PRIMARY KEY (`labelid`);
+
+--
+-- Tablo için indeksler `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`ProjectId`);
+
+--
+-- Tablo için indeksler `projecttype`
+--
+ALTER TABLE `projecttype`
+  ADD PRIMARY KEY (`ProjectTypeId`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `label`
+--
+ALTER TABLE `label`
+  MODIFY `labelid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Tablo için AUTO_INCREMENT değeri `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `ProjectId` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Tablo için AUTO_INCREMENT değeri `projecttype`
+--
+ALTER TABLE `projecttype`
+  MODIFY `ProjectTypeId` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
